9702ddc0c040a5d693abca262e587f561f5210c7
Nikket98 <latkeniket@gmail.com>
2023-04-19 18:44:57 +0000

    product images

--- TechRate/settings.py
+++ TechRate/settings.py
@@ -131,13 +131,13 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
