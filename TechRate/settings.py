05e915b01affb8b74e34218d453fe41856ba4acb
Nikket98 <latkeniket@gmail.com>
2023-04-23 15:17:43 +0000

    settings.py

--- TechRate/settings.py
+++ TechRate/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-from storages.backends.azure_storage import AzureStorage
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'WEBSITE_HOSTNAME' not in os.environ
 
